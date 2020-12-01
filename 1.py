12eed4a24f29473c8e066c01294690072e8e8db5
Zechw <zechwilliams@gmail.com>
2020-12-01 11:01:43 -0800

    1.2

--- 1.py
+++ 1.py
@@ -1,7 +1,16 @@
 
-def fix_expenses(expenses):
+def find_two_compliments(expenses, target=2020):
     while expenses:
         x = expenses.pop()
-        c = 2020 - x
+        c = target - x
         if c in expenses:
-            return c * x
+            return (c, x)
+
+
+def find_three_compliments(expenses, target=2020):
+    while expenses:
+        x = expenses.pop()
+        c = target - x
+        c2 = find_two_compliments(expenses[:], c) # copy so we dont prune off OG expenses
+        if c2:
+            return (x,  c2[0], c2[1])
