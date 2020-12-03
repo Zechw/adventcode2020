ff158d9a9c55fff02870acf5ee3eaff29b47a0d9
Zechw <zechwilliams@gmail.com>
2020-12-03 11:25:07 -0800

    3 cleanup scratch

--- 3.scratch.py
+++ 3.scratch.py
@@ -1,55 +1,4 @@
 
-def count_trees(map, rise=1, run=3):
-    i = 0
-    count = 0
-    for row in map[::rise]:
-        if row[i] == "#":
-            count += 1
-        i = (i+run) % 31
-    return count
-
-def mult_slopes(map_string):
-    map = map_string.split("\n")
-    return (
-        count_trees(map, 1, 1),
-        count_trees(map, 1, 3),
-        count_trees(map, 1, 5),
-        count_trees(map, 1, 7),
-        count_trees(map, 2, 1)
-    )
-
-def variable_mult_slopes(map_string, rise_run_pairs):
-    map = map_string.split("\n")
-    results = []
-    for rise, run in rise_run_pairs:
-        results.append(count_trees(map, rise, run))
-    return results
-
-
-
-def one_pass(map, rise_run_pairs):
-    counts = [0] * len(rise_run_pairs)
-    for i, row in enumerate(map):
-        for j, (rise, run) in enumerate(rise_run_pairs):
-            if i % rise == 0:
-                if row[(i // rise * run) % 31] == "#":
-                    counts[j] += 1
-    return counts
-
-def ops(map_string):
-    map = map_string.split("\n")
-    return one_pass(map, [(1, 1),(1, 3),(1, 5),(1, 7),(2, 1)])
-
-
-def variable_ops(map_string, rise_run_pairs):
-    map = map_string.split("\n")
-    return one_pass(map, rise_run_pairs)
-
-
-
-
-
-
 rr_pairs = []
 for i in range(1,100):
     for j in range(1,100):
@@ -59,16 +8,8 @@ timeit.timeit(lambda: variable_mult_slopes(large_x, rr_pairs), number=10)
 timeit.timeit(lambda: variable_ops(large_x, rr_pairs), number=10)
 
 
-
-
-
-
-
-
-
 timeit.timeit(lambda: mult_slopes(large_x), number=1000)
 timeit.timeit(lambda: ops(large_x), number=1000)
-
 
 
 timeit.timeit(lambda: int(2 * 1 / 2), number=1000000)
@@ -84,7 +25,6 @@ timeit.timeit(lambda: (56465465 // 2 * 3) % 31, number=1000000)
 
 timeit.timeit(lambda: count_trees(m, 1, 3), number=1000)
 timeit.timeit(lambda: bad_one_pass(m, 1, 3), number=1000)
-
 
 
 
