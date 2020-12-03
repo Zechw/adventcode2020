99ecb9081213732569f153c69ac4034fa19a94fc
Zechw <zechwilliams@gmail.com>
2020-12-02 23:38:45 -0800

    3 fix rise-run and speed up op

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 
-def count_trees(map, run=3, rise=1):
+def count_trees(map, rise=1, run=3):
     i = 0
     count = 0
     for row in map[::rise]:
@@ -13,10 +13,10 @@ def mult_slopes(map_string):
     map = map_string.split("\n")
     return (
         count_trees(map, 1, 1),
-        count_trees(map, 3, 1),
-        count_trees(map, 5, 1),
-        count_trees(map, 7, 1),
-        count_trees(map, 1, 2)
+        count_trees(map, 1, 3),
+        count_trees(map, 1, 5),
+        count_trees(map, 1, 7),
+        count_trees(map, 2, 1)
     )
 
 def one_pass(map, rise_run_pairs):
@@ -24,6 +24,10 @@ def one_pass(map, rise_run_pairs):
     for i, row in enumerate(map):
         for j, (rise, run) in enumerate(rise_run_pairs):
             if i % rise == 0:
-                if row[int(i * run / rise) % 31] == "#":
+                if row[(i // rise * run) % 31] == "#":
                     counts[j] += 1
     return counts
+
+def ops(map_string):
+    map = map_string.split("\n")
+    return one_pass(map, [(1, 1),(1, 3),(1, 5),(1, 7),(2, 1)])
