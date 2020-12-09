a8643481f1dc4abb8d8b4abef31618cf126a8395
Zechw <zechwilliams@gmail.com>
2020-12-08 22:52:19 -0800

    9.2

--- 9.py
+++ 9.py
@@ -1,6 +1,7 @@
+parse_nums = lambda inp: [int(x) for x in inp.split("\n")]
 
 def find_first_error(input_text, preamble_length=25):
-    nums = [int(x) for x in input_text.split("\n")]
+    nums = parse_nums(input_text)
     previous_numbers = [] # list of preamble_length previously seen numbers
     sum_window = [] # list of preamble_length-1! currently valid sums. removes & adds preamble_length-1 vals each cycle*(sum_drop)
     sum_drop = 0 # keeps track of how many sums to drop from the window
@@ -20,3 +21,19 @@ def find_first_error(input_text, preamble_length=25):
             sum_window.append(x + n)
         previous_numbers.append(n)
     return False
+
+
+def find_consecutive_sums(input_text, target):
+    nums = parse_nums(input_text)
+    sum_window = []
+    current_sum = 0
+    while True:
+        if current_sum > target:
+            current_sum -= sum_window.pop(0)
+        elif current_sum < target:
+            x = nums.pop(0)
+            current_sum += x
+            sum_window.append(x)
+
+        if current_sum == target:
+            return min(sum_window) + max(sum_window)
