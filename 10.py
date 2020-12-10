f73346d349b137a3c7acfc9dab7e92da03d783ad
Zechw <zechwilliams@gmail.com>
2020-12-10 13:17:35 -0800

    10.2

--- 10.py
+++ 10.py
@@ -1,5 +1,6 @@
-parse_nums = lambda inp: [int(x) for x in inp.split("\n")]
+from collections import defaultdict
 
+parse_nums = lambda inp: [int(x) for x in inp.split("\n")]
 
 def count_jolts(input_string):
     # !! not including last jump to device
@@ -10,3 +11,13 @@ def count_jolts(input_string):
         diff[adp - current_jolt - 1] += 1
         current_jolt = adp
     return diff
+
+def count_perms(input_string):
+    adps = [0] + sorted(parse_nums(input_string))
+    c = defaultdict(int) # known counts dicitonary
+    c[max(adps) + 3] = 1 # init the device
+    while adps:
+        adp = adps.pop()
+        perms = c[adp+1] + c[adp+2] + c[adp+3]
+        c[adp] = perms
+    return c[0]
