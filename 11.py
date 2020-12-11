538a0ce23cc752c702bc1cae11e47c54e61be7b3
Zechw <zechwilliams@gmail.com>
2020-12-10 23:45:27 -0800

    11.2

--- 11.py
+++ 11.py
@@ -17,21 +17,35 @@ def find_stable(inp):
     size = (len(ferry), len(ferry[0]))
     while True:
         next_ferry = deepcopy(ferry)
+
         for i in range(size[0]):
             for j in range(size[1]):
                 seat = ferry[i][j]
                 n_occ = 0
-                for ii, jj in NEIGHBOR_MAP:
-                    ii += i
-                    jj += j
-                    if ii < 0 or jj < 0 or ii == size[0] or jj == size[1]:
-                        continue
-                    if ferry[ii][jj] == "#":
+                for direction in NEIGHBOR_MAP:
+                    n = get_first_seat(direction, i, j, ferry, size)
+                    if n == "#":
                         n_occ += 1
+
                 if seat == "L" and n_occ == 0:
                     next_ferry[i][j] = "#"
-                elif seat == "#" and n_occ > 3:
+                elif seat == "#" and n_occ > 4:
                     next_ferry[i][j] = "L"
+
         if ferry == next_ferry:
             return sum(map(lambda x: x.count("#"), ferry))
         ferry = next_ferry
+
+
+def get_first_seat(direction, i, j, ferry, size):
+    ii = i + direction[0]
+    jj = j + direction[1]
+
+    if ii < 0 or jj < 0 or ii == size[0] or jj == size[1]:
+        return False
+
+    seat = ferry[ii][jj]
+    if seat == ".":
+        return get_first_seat(direction, ii, jj, ferry, size)
+    else:
+        return seat
