98aefdc5befda17f2d03cbb2de8c4dc3452b11a9
Zechw <zechwilliams@gmail.com>
2020-12-13 11:54:17 -0800

    13.2 brute force

--- 13.py
+++ 13.py
@@ -1,5 +1,5 @@
 
-
+# part 1
 def find_bus(inp):
     depart, bus_inp = inp.split("\n", 1)
     depart = int(depart)
@@ -12,3 +12,26 @@ def find_bus(inp):
             best_bus = bus
             wait = w
     return (best_bus, wait, best_bus * wait)
+
+
+
+
+# part 2
+def win_contest(inp, guess_start=1e14):
+    buses = [None if x == "x" else int(x) for x in inp.split("\n")[1].split(",")]
+    first_bus = buses[0]
+    guess = (guess_start // first_bus) * first_bus # start us around the range of gs
+    while True:
+        if verify_guess(guess, buses):
+            return guess
+        guess += first_bus
+
+
+def verify_guess(guess, buses):
+    for bus in buses[1:]: # skip verifying first
+        guess += 1
+        if bus is None:
+            continue
+        if guess % bus != 0:
+            return False
+    return True
