459a1a6721724d0bd6eb756af9dab0b19c982f95
Zechw <zechwilliams@gmail.com>
2020-12-14 20:07:54 -0800

    13 parallel but garbage

--- 13.py
+++ 13.py
@@ -38,7 +38,7 @@ def verify_guess(guess, buses):
 
 
 def win_2(inp, guess_start=1e14):
-    bus_map = [(i, int(x)) for i,x in enumerate(inp.split("\n")[1].split(",")) if x is not "x"]
+    bus_map = [(i, int(x)) for i,x in enumerate(inp.split("\n")[1].split(",")) if x != "x"]
     first_bus = bus_map.pop(0)[1]
     guess = (guess_start // first_bus) * first_bus # start us around the range of gs
     while True:
@@ -51,9 +51,44 @@ def fast_verify(guess, bus_map):
     for i, bus in bus_map:
         if (guess + i) % bus != 0:
             return False
-    return True
+    return guess
 
 
-def win_3(inp, guess_start):
-    # use max(bus) as the guess increment? does this even work, or do we need to
-    # use max(matched) as increment? 
+from functools import reduce
+from multiprocessing  import Pool
+
+def win_3(inp, guess_start=1e14, paralell_factor=100, thread_count=8):
+    # use max(bus) as the guess increment
+    # use multiprocessing (WIP)
+    bus_map = [(i, int(x)) for i,x in enumerate(inp.split("\n")[1].split(",")) if x != "x"]
+    max_bus = reduce(lambda x, y: x if x[1] > y[1] else y, bus_map)[1]
+    guess = (guess_start // max_bus) * max_bus
+    guess_inc = max_bus * paralell_factor
+    with Pool(thread_count) as p:
+        while True:
+            ## !! can't pickle (and thread) a lambda
+            it = [(guess + i, bus_map) for i in range(0, guess_inc, max_bus)]
+            results = p.starmap(fast_verify, it)
+            if any(results):
+                return [r for r in results if r != False]
+            guess += guess_inc
+
+
+
+## ??
+def curry_fv(guess, bus_map):
+    def inner(i):
+        return fast_verify(guess + i, bus_map)
+    return inner
+
+
+
+def test_curry(a,b):
+    def inner(c):
+        return a+b+c
+    return inner
+
+def test_mp():
+    with Pool(8) as p:
+        f = test_curry(3,5)
+        return p.map(f, range(0,10,2))
