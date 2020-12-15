b66fa39a87c21d2e72972ccc1a011b0e1a6a4bec
Zechw <zechwilliams@gmail.com>
2020-12-14 19:39:42 -0800

    14.2

--- 14.py
+++ 14.py
@@ -1,6 +1,6 @@
 
 
-def dock(inp):
+def dock_v1(inp):
     instructions = inp.split("\n")
     mem = {}
     for ins in instructions:
@@ -21,3 +21,34 @@ def mask_val(mask, val):
         else:
             binary[i] = x
     return int(''.join(binary), 2)
+
+
+
+def dock_v2(inp):
+    instructions = inp.split("\n")
+    mem = {}
+    for ins in instructions:
+        cmd, val = ins.split(" = ", 1)
+        if cmd == "mask":
+            mask = val
+        else:
+            mem_loc = cmd.replace("mem[", "").replace("]", "")
+            masked_mem_loc = mask_mem_loc(mask, mem_loc)
+            recursively_write_masked_mem(mem, masked_mem_loc, int(val))
+    return sum(mem.values())
+
+def mask_mem_loc(mask, mem_loc):
+    binary = list(format(int(mem_loc), '036b'))
+    for i, x in enumerate(mask):
+        if x != "0":
+            binary[i] = x
+    return ''.join(binary)
+
+
+def recursively_write_masked_mem(mem, masked_mem_loc, val):
+    fork = masked_mem_loc.find('X')
+    if fork == -1:
+        mem[masked_mem_loc] = val
+    else:
+        recursively_write_masked_mem(mem, masked_mem_loc.replace('X', '0', 1), val)
+        recursively_write_masked_mem(mem, masked_mem_loc.replace('X', '1', 1), val)
